662a4d3e19daf05dd82b0c48488e6c1d19769e42
Chang <ming623@gmail.com>
2017-10-12 19:08:24 +0800

    fix check helmet

--- Assets/Scripts/PlayerData_SetViveDevice.cs
+++ Assets/Scripts/PlayerData_SetViveDevice.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
 public class PlayerData_SetViveDevice : MonoBehaviour {
+
+    public bool isValid { get; private set; }
+    private IEnumerator coroutine;
 
     void OnEnable()
     {
         if (gameObject.name.Contains("Camera"))
         {
-            PlayerDataControl.instance.isHelmet = true;
+            newPosesAction.enabled = true;
+            coroutine = WaitForCheckDeviceActivity(1.0f);
+            StartCoroutine(coroutine);
         }
         else if(gameObject.name.Contains("Controller (right)"))
         {
@@ -20,11 +26,45 @@ public class PlayerData_SetViveDevice : MonoBehaviour {
     {
         if (gameObject.name.Contains("Camera"))
         {
-            PlayerDataControl.instance.isHelmet = false;
+            newPosesAction.enabled = false;
+            StopCoroutine(coroutine);
         }
         else if (gameObject.name.Contains("Controller (right)"))
         {
             PlayerDataControl.instance.isTracker = false;
+        }
+    }
+
+    private void OnNewPoses(TrackedDevicePose_t[] poses)
+    {
+        isValid = false;
+
+        if (!poses[0].bDeviceIsConnected)
+        {
+            return;
+        }
+
+        if (!poses[0].bPoseIsValid)
+        {
+            return;
+        }
+
+        isValid = true;
+    }
+
+    SteamVR_Events.Action newPosesAction;
+
+    PlayerData_SetViveDevice()
+    {
+        newPosesAction = SteamVR_Events.NewPosesAction(OnNewPoses);
+    }
+
+    IEnumerator WaitForCheckDeviceActivity(float waitTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            PlayerDataControl.instance.isHelmet = isValid;
         }
     }
 }
